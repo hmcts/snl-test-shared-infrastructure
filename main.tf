98bf92562cbf4be60074be4d465893b2876deb67
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-02-12 15:38:44 +0000

    Added KV

--- main.tf
+++ main.tf
@@ -14,3 +14,16 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.common_tags
 }
+
+module "kv" {
+  count                   = var.env == "stg" ? 1 : 0
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = local.key_vault_name
+  product                 = var.product
+  env                     = var.env
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = var.active_directory_group
+  common_tags             = var.common_tags
+  create_managed_identity = false
+}
