0798e91c7d5b3704443f4b9979ba8e0bfe8a0f6b
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-02-12 11:26:45 +0000

    Updated main tf

--- main.tf
+++ main.tf
@@ -7,7 +7,9 @@ locals {
 
 data "azurerm_client_config" "current" {}
 
+//At present, this will only create the resource group in the staging environment
 resource "azurerm_resource_group" "rg" {
+  count    = var.env == "stg" ? 1 : 0
   name     = local.resource_group_name
   location = var.location
   tags     = var.common_tags
